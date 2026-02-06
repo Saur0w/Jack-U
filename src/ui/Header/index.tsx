f4d3b4696cf9e69bdec91c1de0c12ca5649aa12a
saur0w <sthapliyal085@gmail.com>
2026-02-06 21:59:43 +0530

    "Header"

--- src/ui/Header/index.tsx
+++ src/ui/Header/index.tsx
@@ -10,6 +10,8 @@ import styles from "./style.module.scss";
 export default function Header() {
     const hamburgerRef = useRef<HTMLDivElement | null>(null);
     const [isOpen, setIsOpen] = useState(false);
+    const headerRef = useRef<HTMLDivElement | null>(null);
+
     const tl = useRef<gsap.core.Timeline | null>(null);
 
     const toggleMenu = () => {
@@ -27,15 +29,29 @@ export default function Header() {
            tl.current.to(lines[0], { y: 10, rotate: 45, duration: 0.3, ease: "power2.out" }, 0)
                .to(lines[1], { opacity: 0, duration: 0.2 }, 0)
                .to(lines[2], { y: -10, rotate: -45, duration: 0.3, ease: "power2.out" }, 0);
+
+           gsap.to(headerRef.current, {
+               left: 50,
+               right: 50,
+               ease: "power2.out"
+           });
+
+
         } else {
             tl.current.to(lines[0], { y: 0, rotate: 0, duration: 0.3, ease: "power2.out" }, 0)
                 .to(lines[1], { opacity: 1, duration: 0.2 }, 0)
                 .to(lines[2], { y: 0, rotate: 0, duration: 0.3, ease: "power2.out" }, 0);
+
+            gsap.to(headerRef.current, {
+                left: 400,
+                right: 400,
+                ease: "power2.out"
+            });
         }
 
     }, { dependencies: [isOpen]});
     return (
-        <header className={`fixed top-10 left-130 right-130 h-20 rounded-[10px] ${styles.header}`}>
+        <header className={`fixed top-10  h-20 rounded-[10px] ${styles.header}`} ref={headerRef}>
             <div className={styles.topBar}>
                 <div className={styles.hamburger} ref={hamburgerRef} onClick={toggleMenu} >
                     <span></span>
